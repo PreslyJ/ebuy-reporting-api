67b0fe2ab9db8c69f58dc42f5ddef935bd53b97d
presly <presly>
2017-12-10 08:45:43 +0530

    build

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+git pull origin master
+git submodule foreach git checkout master &&  git pull origin master
+mvn clean install
 docker build --force-rm -t node1:5000/ebuy-reporting-api .
 docker push node1:5000/ebuy-reporting-api
